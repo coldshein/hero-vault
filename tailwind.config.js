2a307f9f533e51077c3de34d6cec80c990869d17
Roman Bekh <coldshein@gmail.com>
2024-10-26 05:18:20 +0300

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        "white-bg": "#f1f1f1", // Основний темний фон
-        "main-text": "#000000", // Колір тексту
-        "accent": "#C33C54", // Акцентний колір
+        "white-bg": "#f1f1f1",
+        "main-text": "#000000",
+        "accent": "#C33C54",
         "secondary-accent": "#1876D1",
-        "secondary-bg": "#FFFFFF", // Додатковий фон
-
-        "dark-bg": "#0E1117",
-        "dark-text": "#F0F6FB",
-        "":"",
-        "":"",
-        "dark-secondary-bg": "#151B23",
+        "secondary-bg": "#FFFFFF",
       },
     },
   },
